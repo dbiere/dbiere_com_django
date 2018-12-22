b5ae4ec90a34fccff7d6a27e7933e000381f3782
devonbiere <dbiere@gmail.com>
2018-12-22 11:14:38 -0500

    Added ordering, filtering, and display enhancements to admin pages

--- books/admin.py
+++ books/admin.py
@@ -5,5 +5,23 @@ from .models import Book
 
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    list_per_page = 200
+    search_fields = ('display_name',)
+    
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'show_authors')
+    list_filter = ('author__formal_sort_name',)
+    list_per_page = 200
+    ordering = ('title',)
+    search_fields = ('title', 'author__display_name')
+
+    # Have to do this to show many-to-many objects
+    def show_authors(self, obj):
+        return ", ".join([a.formal_sort_name for a in obj.author.all()])
+    
+    show_authors.short_description = 'Author(s)'
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- movies/admin.py
+++ movies/admin.py
@@ -4,4 +4,34 @@ from .models import Movie
 
 # Register your models here.
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'year_released', 'is_favorite',)
+    list_editable = ('is_favorite',)
+    list_filter = ('is_favorite', 'year_released',)
+    list_per_page=200
+    ordering = ('title',)
+    search_fields = ('title',)
+
+admin.site.register(Movie, MovieAdmin)
+
+
+'''
+from django.contrib import admin
+
+from .models import Listing
+
+# This lets us add filters, etc. to main Listing admin page
+# Important: remember to add this to the model registration below if you do this
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+# Register models
+admin.site.register(Listing, ListingAdmin)
+
+'''
