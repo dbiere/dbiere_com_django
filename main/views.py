0c2ddaf8cab242f0c5d67e578c9649de2a82386c
devonbiere <dbiere@gmail.com>
2018-12-22 12:43:03 -0500

    Updated Media Diet main page to exclude books I haven't finished

--- main/views.py
+++ main/views.py
@@ -14,7 +14,7 @@ def learning(request):
 def media_diet(request):
 
     recent_items_limit = 10
-    recent_books = Book.objects.all().order_by('-date_finished')[:recent_items_limit]
+    recent_books = Book.objects.all().exclude(date_finished__isnull=True).order_by('-date_finished')[:recent_items_limit]
     recent_movies = Movie.objects.all().order_by('-date_watched')[:recent_items_limit]
 
     context = {
