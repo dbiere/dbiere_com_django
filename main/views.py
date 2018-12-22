aa010d7300c3f7257fd220f883746028b1ba39b4
devonbiere <dbiere@gmail.com>
2018-12-22 12:39:29 -0500

    Updated Media Diet main page to include book and movie lists. Added explanation

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from movies.models import Movie
+from books.models import Book
+
 def index(request):
     return render(request, 'main/index.html')
 
@@ -9,7 +12,17 @@ def learning(request):
     return render(request, 'main/learning.html')
 
 def media_diet(request):
-    return render(request, 'main/media_diet.html')
+
+    recent_items_limit = 10
+    recent_books = Book.objects.all().order_by('-date_finished')[:recent_items_limit]
+    recent_movies = Movie.objects.all().order_by('-date_watched')[:recent_items_limit]
+
+    context = {
+        'recent_books': recent_books,
+        'recent_movies': recent_movies,
+    }
+
+    return render(request, 'main/media_diet.html', context)
 
 def resume(request):
     return render(request, 'main/resume.html')
